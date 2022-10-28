95b26e0dc0eb7e1afbede8a6a4e4408a1dba6dca
Lucas Landim <lucas.rodrigues@sptech.school>
2022-10-28 20:16:42 -0300

    realizada a configuração da tela de dependentes

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -98,9 +98,50 @@ function cadastrar(req, res) {
     }
 }
 
+function cadastrarDependente(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var cargo = req.body.cargoServer;
+    var nome = req.body.nomeServer;
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+    var idAdmin = req.body.idAdminServer;
+
+    // Faça as validações dos valores
+    if (nome == undefined) {
+        res.status(400).send("Seu nome está undefined!");
+    } else if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (cargo == undefined) {
+        res.status(400).send("Seu cargo está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está undefined!");
+    } else if (idAdmin == undefined) {
+        res.status(400).send("Sua id de administrador está undefined!");
+    } else {
+        
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.cadastrarDependente(cargo, nome, email, senha, idAdmin)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
 module.exports = {
     entrar,
     cadastrar,
+    cadastrarDependente,
     listar,
     testar
 }
--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -31,8 +31,21 @@ function cadastrar(nome, email, cnpj, senha) {
     return database.executar(instrucao);
 }
 
+function cadastrarDependente(cargo, nome, email, senha, idAdmin) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",idAdmin, nome, email, senha, cargo);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucao = `
+        INSERT INTO usuario (fkAdministrador, nomeUsuario, email, cargo, senha) VALUES (${idAdmin}, '${nome}', '${email}', '${cargo}', '${senha}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
 module.exports = {
     entrar,
     cadastrar,
+    cadastrarDependente,
     listar,
 };
