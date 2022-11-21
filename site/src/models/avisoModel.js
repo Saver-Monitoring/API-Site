a718605c6b8a14424c6b75da0d8c07bf5dc591ab
Lucas Landim <lucas.rodrigues@sptech.school>
2022-11-20 22:25:49 -0300

    backend dos gráficos completo

--- site/src/models/avisoModel.js
+++ site/src/models/avisoModel.js
@@ -1,20 +1,16 @@
 var database = require("../database/config");
 
-function listar() {
+function listar(idUsuario) {
     console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucao = `
-        SELECT 
-            a.id AS idAviso,
-            a.titulo,
-            a.descricao,
-            a.fk_usuario,
-            u.id AS idUsuario,
-            u.nome,
-            u.email,
-            u.senha
-        FROM aviso a
-            INNER JOIN usuario u
-                ON a.fk_usuario = u.id;
+    SELECT 
+    rack.idRack,
+    computador.idComputador
+        FROM computador 
+            inner join rack on rack.idRack = computador.fkRack 
+            inner join empresa on empresa.idEmpresa = rack.fkEmpresa 
+            inner join usuario on empresa.idEmpresa = usuario.fkEmpresa 
+        where usuario.email = ${idUsuario}
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -44,19 +40,14 @@ function pesquisarDescricao(texto) {
 function listarPorUsuario(idUsuario) {
     console.log("ACESSEI O AVISO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarPorUsuario()");
     var instrucao = `
-        SELECT 
-            a.id AS idAviso,
-            a.titulo,
-            a.descricao,
-            a.fk_usuario,
-            u.id AS idUsuario,
-            u.nome,
-            u.email,
-            u.senha
-        FROM aviso a
-            INNER JOIN usuario u
-                ON a.fk_usuario = u.id
-        WHERE u.id = ${idUsuario};
+    SELECT 
+    rack.idRack,
+    computador.idComputador
+        FROM computador 
+            inner join rack on rack.idRack = computador.fkRack 
+            inner join empresa on empresa.idEmpresa = rack.fkEmpresa 
+            inner join usuario on empresa.idEmpresa = usuario.fkEmpresa 
+        where usuario.idUsuario = ${idUsuario}
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
